4841fe0ae68ae9320fdc4f03cbdab42d341e2fb4
Cristian <51058790+cgasper79@users.noreply.github.com>
2023-05-03 13:07:21 +0200

    Update main.js

--- CV1/scripts/main.js
+++ CV1/scripts/main.js
@@ -1,4 +1,46 @@
 // Add your javascript here
+
+//calcular la edad de una persona
+//recibe la fecha como un string en formato español
+//devuelve un entero con la edad. Devuelve false en caso de que la fecha sea incorrecta o mayor que el dia actual
+function calcular_edad(fecha){
+    hoy=new Date()
+    var array_fecha = fecha.split("/")  
+    
+    if (array_fecha.length!=3)
+       return false
+  
+    var ano
+    ano = parseInt(array_fecha[2]);
+  
+    if (isNaN(ano))
+       return false
+
+    var mes
+    mes = parseInt(array_fecha[1]);
+    if (isNaN(mes))
+       return false
+
+    var dia
+    dia = parseInt(array_fecha[0]);
+    if (isNaN(dia))
+       return false
+
+    if (ano<=99)
+       ano +=1900
+
+    edad=hoy.getYear()- ano - 1; //-1 porque no se si ha cumplido años ya este año
+
+    if (hoy.getMonth() + 1 - mes < 0) //+ 1 porque los meses empiezan en 0
+       return edad
+    if (hoy.getMonth() + 1 - mes > 0)
+       return edad+1
+
+    if (hoy.getUTCDate() - dia >= 0)
+       return edad + 1
+    return edad
+}
+
 // Don't forget to add it into respective layouts where this js file is needed
 
 
